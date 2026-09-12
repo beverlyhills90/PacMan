e811b6d360beffdc718b8a37478be9c7363e0470
beverlyhills90 <sasadan800@gmail.com>
2026-09-12 17:02:04 +0200

    formating stuff

--- src/parsing.py
+++ src/parsing.py
@@ -236,7 +236,5 @@ def validation(config_path: Path) -> Config:
         raise ParsingError(
             f"Json Error: {e.msg} on line {e.lineno - 1}, column {e.colno}"
         )
-    except UnicodeDecodeError as e:
-        raise ParsingError(
-            f"Error: {e}"
-        )
+    except UnicodeDecodeError:
+        raise ParsingError("Error: config file must be UTF-8 encoded")
