7a5795aa24f850c1a345a162df0660f150559192
beverlyhills90 <sasadan800@gmail.com>
2026-09-12 16:18:11 +0200

    mazeadapter core

--- src/parcing.py
+++ src/parcing.py
@@ -1,7 +1,8 @@
-from pathlib import Path
-from pydantic import BaseModel, ValidationError, Field, model_validator
 import json
+from pathlib import Path
 from typing import Any
+
+from pydantic import BaseModel, Field, ValidationError, model_validator
 
 
 class ParsingError(Exception):
@@ -23,13 +24,15 @@ class Config(BaseModel):
     points_per_super_pacgum: Any = Field(default=50)
     points_per_ghost: Any = Field(default=200)
     level_max_time: Any = Field(default=90)
-    levels: Any = Field(default_factory=list)
+    levels: list = Field(default_factory=list)
 
     @model_validator(mode="after")
     def levels_validator(self):
         if type(self.levels) is not list:
-            print(f"Config warning: levels: {self.levels} is not list. "
-                  "Deleting pshhhh.....")
+            print(
+                f"Config warning: levels: {self.levels} is not list. "
+                "Deleting pshhhh....."
+            )
             self.levels = []
 
         self.validate_levels()
@@ -64,10 +67,9 @@ class Config(BaseModel):
     def create_levels(self, missing_levels: int) -> None:
         i = 10 - missing_levels
         while i < 10:
+            level: Level = Level()
             if i == 0:
-                level: Level = Level(seed=42)
-            else:
-                level: Level = Level()
+                level = Level(seed=42)
             self.levels.append(level)
             i += 1
 
@@ -153,7 +155,10 @@ class Config(BaseModel):
                 "integer; using default value 50."
             )
             self.points_per_super_pacgum = 50
-        elif self.points_per_super_pacgum < 25 or self.points_per_super_pacgum > 100:
+        elif (
+            self.points_per_super_pacgum < 25
+            or self.points_per_super_pacgum > 100
+        ):
             print(
                 "Config warning: 'points_per_super_pacgum' must be between "
                 f"25 and 100; got {self.points_per_super_pacgum!r}, "
@@ -222,7 +227,8 @@ def validation(config_path: Path) -> Config:
             print(
                 "Config warning: config must be dict "
                 f"type, its {type(json_config)}. "
-                "Setting everything to default.")
+                "Setting everything to default."
+            )
             json_config = {}
         config = Config.model_validate(json_config)
         return config
@@ -232,4 +238,5 @@ def validation(config_path: Path) -> Config:
         raise ParsingError(f"Error: {e} '{target}'")
     except json.JSONDecodeError as e:
         raise ParsingError(
-            f"Json Error: {e.msg} on line {e.lineno - 1}, column {e.colno}")
+            f"Json Error: {e.msg} on line {e.lineno - 1}, column {e.colno}"
+        )
