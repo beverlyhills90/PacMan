07b8de648ffda0a469a0aa05b36aae454577041f
faronbleed <yaroslav.popovych17@gmail.com>
2026-09-17 15:32:58 +0200

    up to date

--- src/visuals/visualiser.py
+++ src/visuals/visualiser.py
@@ -49,7 +49,7 @@ class Visualiser():
 
         while (True):
             snapshot = self.game.snapshot()
-            print(snapshot.status)
+            print(self.state)
             if snapshot.status == "level_won":
                 self.set_new_level()
             if snapshot.status == "victory":
