c868ca6dd59cc5fb5c33f05733ea30aafb0ce440
faronbleed <yaroslav.popovych17@gmail.com>
2026-09-22 20:32:43 +0200

    added control view

--- src/visuals/event_handler.py
+++ src/visuals/event_handler.py
@@ -4,15 +4,18 @@ from shared_types import VisualState, Direction
 from game import Game
 from .menu_view import MenuView
 from .highscore_view import HighscoreView
+from .controls_view import ControlsView
 
 
 class EventHandler():
     def __init__(self, screen: pg.Surface, game: Game,
-                 menu_view: MenuView, highscore_view: HighscoreView) -> None:
+                 menu_view: MenuView, highscore_view: HighscoreView,
+                 controls_view: ControlsView) -> None:
         self.screen: pg.Surface = screen
         self.game: Game = game
         self.menu_view: MenuView = menu_view
         self.highscore_view: HighscoreView = highscore_view
+        self.controls_view: ControlsView = controls_view
 
     def event_handling(self, dt: float, state: VisualState) -> VisualState | None:
         for event in pg.event.get():
@@ -25,6 +28,8 @@ class EventHandler():
                 return self.highscore_events(event)
             if state == "victory_screen" and event.type == pg.KEYDOWN:
                 return self.victory_events()
+            if state == "controls" and event.type == pg.MOUSEBUTTONUP:
+                return self._control_menu_events(event)
 
         if state == "playing":
             return self.game_events(dt)
@@ -44,7 +49,7 @@ class EventHandler():
             intent = "right"
         if cheats[pg.K_l]:
             self.game.cheat("level_skip")
-        if cheats[pg.K_i]:
+        if cheats[pg.K_g]:
             self.game.cheat("inflives")
         if cheats[pg.K_1]:
             self.game.cheat("pluslive")
@@ -65,6 +70,12 @@ class EventHandler():
         mouse_pos = event.pos
         if event.button == 1:
             return (self.highscore_view.handle_input(mouse_pos))
+        return None
+
+    def _control_menu_events(self, event: pg.Event) -> VisualState | None:
+        mouse_pos = event.pos
+        if event.button == 1:
+            return self.controls_view.handle_input(mouse_pos)
         return None
 
     def victory_events(self) -> VisualState | None:
