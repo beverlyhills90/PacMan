f36dda3321aa4a69db5ab49ea784d1059820d951
beverlyhills90 <sasadan800@gmail.com>
2026-09-24 10:21:04 +0200

    rm debug print

--- src/visuals/highscore_view.py
+++ src/visuals/highscore_view.py
@@ -82,7 +82,7 @@ class HighscoreView:
         button_fitting: list[str] = []
         i = 0
         for word in message_list:
-            print(word)
+            #print(word)
             button_fitting.append(word)
             str_surface = self.fonts.small_button_font.render(
                 "".join(button_fitting), False, "white"
